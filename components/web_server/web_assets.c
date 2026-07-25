b23f54bb891456ececcb75f066852f0884dcebd6
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-26 00:03:52 +0500

    feat(web): embed split HTML CSS and JavaScript assets

--- components/web_server/web_assets.c
+++ components/web_server/web_assets.c
@@ -3,9 +3,28 @@
 
 extern const uint8_t index_html_start[] asm("_binary_index_html_start");
 extern const uint8_t index_html_end[] asm("_binary_index_html_end");
+extern const uint8_t app_css_start[] asm("_binary_app_css_start");
+extern const uint8_t app_css_end[] asm("_binary_app_css_end");
+extern const uint8_t app_js_start[] asm("_binary_app_js_start");
+extern const uint8_t app_js_end[] asm("_binary_app_js_end");
+
+static const char *asset(const uint8_t *start, const uint8_t *end, size_t *length)
+{
+    if (length) *length = (size_t)(end - start);
+    return (const char *)start;
+}
 
 const char *web_assets_index(size_t *length)
 {
-    if (length) *length = (size_t)(index_html_end - index_html_start);
-    return (const char *)index_html_start;
+    return asset(index_html_start, index_html_end, length);
+}
+
+const char *web_assets_css(size_t *length)
+{
+    return asset(app_css_start, app_css_end, length);
+}
+
+const char *web_assets_js(size_t *length)
+{
+    return asset(app_js_start, app_js_end, length);
 }
