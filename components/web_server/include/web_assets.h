f80b38fd03a6dd3199a8b59142d927d5fd6fefbe
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-26 10:24:47 +0500

    build: expose Wi-Fi credential guard asset

--- components/web_server/include/web_assets.h
+++ components/web_server/include/web_assets.h
@@ -6,4 +6,5 @@ const char *web_assets_css(size_t *length);
 const char *web_assets_wifi_css(size_t *length);
 const char *web_assets_js(size_t *length);
 const char *web_assets_wifi_utils_js(size_t *length);
+const char *web_assets_wifi_guard_js(size_t *length);
 const char *web_assets_wifi_js(size_t *length);
