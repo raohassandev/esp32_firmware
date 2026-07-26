5a894dc8bc6167f38b1515e5535448d8448ac3d7
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-26 10:44:07 +0500

    Add inverter command diagnostics

--- components/inverter_manager/include/inverter_types.h
+++ components/inverter_manager/include/inverter_types.h
@@ -7,5 +7,9 @@ typedef struct {
     float commanded_percent;
     float commanded_power_kw;
     bool online;
+    bool has_command;
+    uint32_t last_command_ms;
+    uint32_t write_successes;
     uint32_t write_errors;
+    int32_t last_error;
 } inverter_data_t;
