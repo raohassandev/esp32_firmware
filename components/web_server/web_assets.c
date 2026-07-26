290f8b4f47fe3b519ed82fe156d5e428ee64cb47
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-26 11:44:25 +0500

    Embed operational telemetry assets

--- components/web_server/web_assets.c
+++ components/web_server/web_assets.c
@@ -7,6 +7,8 @@ extern const uint8_t app_css_start[] asm("_binary_app_css_start");
 extern const uint8_t app_css_end[] asm("_binary_app_css_end");
 extern const uint8_t wifi_css_start[] asm("_binary_wifi_css_start");
 extern const uint8_t wifi_css_end[] asm("_binary_wifi_css_end");
+extern const uint8_t operational_css_start[] asm("_binary_operational_css_start");
+extern const uint8_t operational_css_end[] asm("_binary_operational_css_end");
 extern const uint8_t app_js_start[] asm("_binary_app_js_start");
 extern const uint8_t app_js_end[] asm("_binary_app_js_end");
 extern const uint8_t wifi_utils_js_start[] asm("_binary_wifi_utils_js_start");
@@ -15,48 +17,23 @@ extern const uint8_t wifi_guard_js_start[] asm("_binary_wifi_guard_js_start");
 extern const uint8_t wifi_guard_js_end[] asm("_binary_wifi_guard_js_end");
 extern const uint8_t wifi_js_start[] asm("_binary_wifi_js_start");
 extern const uint8_t wifi_js_end[] asm("_binary_wifi_js_end");
+extern const uint8_t operational_js_start[] asm("_binary_operational_js_start");
+extern const uint8_t operational_js_end[] asm("_binary_operational_js_end");
 
 static const char *asset(const uint8_t *start, const uint8_t *end, size_t *length)
 {
     size_t size = (size_t)(end - start);
-    if (size > 0 && start[size - 1] == '\0') {
-        --size;
-    }
+    if (size > 0 && start[size - 1] == '\0') --size;
     if (length) *length = size;
     return (const char *)start;
 }
 
-const char *web_assets_index(size_t *length)
-{
-    return asset(index_html_start, index_html_end, length);
-}
-
-const char *web_assets_css(size_t *length)
-{
-    return asset(app_css_start, app_css_end, length);
-}
-
-const char *web_assets_wifi_css(size_t *length)
-{
-    return asset(wifi_css_start, wifi_css_end, length);
-}
-
-const char *web_assets_js(size_t *length)
-{
-    return asset(app_js_start, app_js_end, length);
-}
-
-const char *web_assets_wifi_utils_js(size_t *length)
-{
-    return asset(wifi_utils_js_start, wifi_utils_js_end, length);
-}
-
-const char *web_assets_wifi_guard_js(size_t *length)
-{
-    return asset(wifi_guard_js_start, wifi_guard_js_end, length);
-}
-
-const char *web_assets_wifi_js(size_t *length)
-{
-    return asset(wifi_js_start, wifi_js_end, length);
-}
+const char *web_assets_index(size_t *length) { return asset(index_html_start, index_html_end, length); }
+const char *web_assets_css(size_t *length) { return asset(app_css_start, app_css_end, length); }
+const char *web_assets_wifi_css(size_t *length) { return asset(wifi_css_start, wifi_css_end, length); }
+const char *web_assets_operational_css(size_t *length) { return asset(operational_css_start, operational_css_end, length); }
+const char *web_assets_js(size_t *length) { return asset(app_js_start, app_js_end, length); }
+const char *web_assets_wifi_utils_js(size_t *length) { return asset(wifi_utils_js_start, wifi_utils_js_end, length); }
+const char *web_assets_wifi_guard_js(size_t *length) { return asset(wifi_guard_js_start, wifi_guard_js_end, length); }
+const char *web_assets_wifi_js(size_t *length) { return asset(wifi_js_start, wifi_js_end, length); }
+const char *web_assets_operational_js(size_t *length) { return asset(operational_js_start, operational_js_end, length); }
