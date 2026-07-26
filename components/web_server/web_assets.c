89bc1d48ac66df1581a1a2ae9c2010bfb3ba4274
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-26 10:14:59 +0500

    build: embed modular Wi-Fi commissioning assets

--- components/web_server/web_assets.c
+++ components/web_server/web_assets.c
@@ -5,8 +5,14 @@ extern const uint8_t index_html_start[] asm("_binary_index_html_start");
 extern const uint8_t index_html_end[] asm("_binary_index_html_end");
 extern const uint8_t app_css_start[] asm("_binary_app_css_start");
 extern const uint8_t app_css_end[] asm("_binary_app_css_end");
+extern const uint8_t wifi_css_start[] asm("_binary_wifi_css_start");
+extern const uint8_t wifi_css_end[] asm("_binary_wifi_css_end");
 extern const uint8_t app_js_start[] asm("_binary_app_js_start");
 extern const uint8_t app_js_end[] asm("_binary_app_js_end");
+extern const uint8_t wifi_utils_js_start[] asm("_binary_wifi_utils_js_start");
+extern const uint8_t wifi_utils_js_end[] asm("_binary_wifi_utils_js_end");
+extern const uint8_t wifi_js_start[] asm("_binary_wifi_js_start");
+extern const uint8_t wifi_js_end[] asm("_binary_wifi_js_end");
 
 static const char *asset(const uint8_t *start, const uint8_t *end, size_t *length)
 {
@@ -28,7 +34,22 @@ const char *web_assets_css(size_t *length)
     return asset(app_css_start, app_css_end, length);
 }
 
+const char *web_assets_wifi_css(size_t *length)
+{
+    return asset(wifi_css_start, wifi_css_end, length);
+}
+
 const char *web_assets_js(size_t *length)
 {
     return asset(app_js_start, app_js_end, length);
+}
+
+const char *web_assets_wifi_utils_js(size_t *length)
+{
+    return asset(wifi_utils_js_start, wifi_utils_js_end, length);
+}
+
+const char *web_assets_wifi_js(size_t *length)
+{
+    return asset(wifi_js_start, wifi_js_end, length);
 }
