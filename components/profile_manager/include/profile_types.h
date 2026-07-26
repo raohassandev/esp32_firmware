92314d81a6745bcc1c10f1873eb3716cb78bcc4e
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-26 11:09:01 +0500

    Define versioned inverter telemetry profiles

--- components/profile_manager/include/profile_types.h
+++ components/profile_manager/include/profile_types.h
@@ -4,6 +4,9 @@
 #include "modbus_types.h"
 
 #define PROFILE_KEY_MAX_LEN 32
+#define PROFILE_MAX_INVERTERS 12
+#define INVERTER_TELEMETRY_PROFILE_MAGIC 0x49545046u
+#define INVERTER_TELEMETRY_PROFILE_VERSION 1u
 
 typedef struct {
     char key[PROFILE_KEY_MAX_LEN];
@@ -16,3 +19,14 @@ typedef struct {
     uint32_t poll_interval_ms;
     bool writable;
 } register_point_t;
+
+typedef struct {
+    bool enabled;
+    register_point_t active_power;
+} inverter_telemetry_profile_t;
+
+typedef struct {
+    uint32_t magic;
+    uint16_t version;
+    inverter_telemetry_profile_t inverters[PROFILE_MAX_INVERTERS];
+} inverter_telemetry_profile_set_t;
