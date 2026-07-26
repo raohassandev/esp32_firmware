4b36446d273df414140e5cc8efb02f65d42e8899
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-26 10:43:03 +0500

    Add meter runtime diagnostics

--- components/meter_manager/include/meter_types.h
+++ components/meter_manager/include/meter_types.h
@@ -6,5 +6,9 @@ typedef struct {
     float active_power_kw;
     bool online;
     uint32_t last_update_ms;
+    uint32_t last_attempt_ms;
+    uint32_t success_count;
     uint32_t response_errors;
+    uint32_t consecutive_failures;
+    int32_t last_error;
 } meter_data_t;
