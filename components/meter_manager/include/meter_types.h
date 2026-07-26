e6838e9f8f47fc96ef7cd5eaa815e4f5b8dc7a0d
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-26 23:00:24 +0500

    Track meter runtime health

--- components/meter_manager/include/meter_types.h
+++ components/meter_manager/include/meter_types.h
@@ -5,6 +5,11 @@
 typedef struct {
     float active_power_kw;
     bool online;
+    bool connection_initialized;
     uint32_t last_update_ms;
+    uint32_t last_attempt_ms;
+    uint32_t success_count;
     uint32_t response_errors;
+    uint32_t consecutive_failures;
+    int32_t last_error;
 } meter_data_t;
