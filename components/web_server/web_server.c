6e5569d7f68b1b16ced9509a873f617bf883a9a9
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-27 16:15:17 +0500

    Register full inverter configuration API and UI asset

--- components/web_server/web_server.c
+++ components/web_server/web_server.c
@@ -6,6 +6,7 @@
 #include "em500_settings_plan_api.h"
 #include "esp_check.h"
 #include "esp_http_server.h"
+#include "inverter_config_api.h"
 #include "inverter_profile_api.h"
 #include "meter_config_api.h"
 #include "web_api.h"
@@ -75,6 +76,7 @@ static esp_err_t js_handler(httpd_req_t *request)
         web_assets_devices_js,
         web_assets_devices_refresh_js,
         web_assets_inverter_profiles_js,
+        web_assets_inverter_config_js,
         web_assets_em500_utils_js,
         web_assets_em500_core_js,
         web_assets_em500_profiles_js,
@@ -90,7 +92,7 @@ esp_err_t web_server_start(void)
 
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     /* 3 asset handlers + core/device/profile/configuration/diagnostic APIs. */
-    config.max_uri_handlers = 24;
+    config.max_uri_handlers = 25;
     config.stack_size = 7168;
     ESP_RETURN_ON_ERROR(httpd_start(&s_server, &config), "web", "HTTP server start failed");
 
@@ -108,6 +110,7 @@ esp_err_t web_server_start(void)
     ESP_RETURN_ON_ERROR(web_api_register(s_server), "web", "core API registration failed");
     ESP_RETURN_ON_ERROR(device_api_register(s_server), "web", "device API registration failed");
     ESP_RETURN_ON_ERROR(inverter_profile_api_register(s_server), "web", "inverter profile API registration failed");
+    ESP_RETURN_ON_ERROR(inverter_config_api_register(s_server), "web", "inverter configuration API registration failed");
     ESP_RETURN_ON_ERROR(meter_config_api_register(s_server), "web", "meter configuration API registration failed");
     ESP_RETURN_ON_ERROR(em500_api_register(s_server), "web", "EM500 snapshot API registration failed");
     ESP_RETURN_ON_ERROR(em500_history_api_register(s_server), "web", "EM500 history API registration failed");
