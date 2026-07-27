bdd8f2cafdf38cdf4f904198f0d5c1c9cfb45d1d
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-27 17:09:40 +0500

    feat: expose embedded theme assets

--- components/web_server/web_assets.c
+++ components/web_server/web_assets.c
@@ -5,6 +5,8 @@ extern const uint8_t index_html_start[] asm("_binary_index_html_start");
 extern const uint8_t index_html_end[] asm("_binary_index_html_end");
 extern const uint8_t app_css_start[] asm("_binary_app_css_start");
 extern const uint8_t app_css_end[] asm("_binary_app_css_end");
+extern const uint8_t theme_css_start[] asm("_binary_theme_css_start");
+extern const uint8_t theme_css_end[] asm("_binary_theme_css_end");
 extern const uint8_t wifi_css_start[] asm("_binary_wifi_css_start");
 extern const uint8_t wifi_css_end[] asm("_binary_wifi_css_end");
 extern const uint8_t devices_css_start[] asm("_binary_devices_css_start");
@@ -13,6 +15,8 @@ extern const uint8_t em500_css_start[] asm("_binary_em500_css_start");
 extern const uint8_t em500_css_end[] asm("_binary_em500_css_end");
 extern const uint8_t app_js_start[] asm("_binary_app_js_start");
 extern const uint8_t app_js_end[] asm("_binary_app_js_end");
+extern const uint8_t theme_js_start[] asm("_binary_theme_js_start");
+extern const uint8_t theme_js_end[] asm("_binary_theme_js_end");
 extern const uint8_t wifi_utils_js_start[] asm("_binary_wifi_utils_js_start");
 extern const uint8_t wifi_utils_js_end[] asm("_binary_wifi_utils_js_end");
 extern const uint8_t wifi_guard_js_start[] asm("_binary_wifi_guard_js_start");
@@ -50,10 +54,12 @@ static const char *asset(const uint8_t *start, const uint8_t *end, size_t *lengt
 
 const char *web_assets_index(size_t *length) { return asset(index_html_start, index_html_end, length); }
 const char *web_assets_css(size_t *length) { return asset(app_css_start, app_css_end, length); }
+const char *web_assets_theme_css(size_t *length) { return asset(theme_css_start, theme_css_end, length); }
 const char *web_assets_wifi_css(size_t *length) { return asset(wifi_css_start, wifi_css_end, length); }
 const char *web_assets_devices_css(size_t *length) { return asset(devices_css_start, devices_css_end, length); }
 const char *web_assets_em500_css(size_t *length) { return asset(em500_css_start, em500_css_end, length); }
 const char *web_assets_js(size_t *length) { return asset(app_js_start, app_js_end, length); }
+const char *web_assets_theme_js(size_t *length) { return asset(theme_js_start, theme_js_end, length); }
 const char *web_assets_wifi_utils_js(size_t *length) { return asset(wifi_utils_js_start, wifi_utils_js_end, length); }
 const char *web_assets_wifi_guard_js(size_t *length) { return asset(wifi_guard_js_start, wifi_guard_js_end, length); }
 const char *web_assets_wifi_js(size_t *length) { return asset(wifi_js_start, wifi_js_end, length); }
