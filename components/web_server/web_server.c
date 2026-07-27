f31f310a77254f98d9cad87f6c0227e5fe40fb3c
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-27 16:59:20 +0500

    feat: serve inverter telemetry browser module

--- components/web_server/web_server.c
+++ components/web_server/web_server.c
@@ -77,6 +77,7 @@ static esp_err_t js_handler(httpd_req_t *request)
         web_assets_devices_refresh_js,
         web_assets_inverter_profiles_js,
         web_assets_inverter_config_js,
+        web_assets_inverter_telemetry_js,
         web_assets_em500_utils_js,
         web_assets_em500_core_js,
         web_assets_em500_profiles_js,
