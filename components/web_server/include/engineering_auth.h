d885dd39a2404b590cc33780696d9e682e931bc6
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-27 18:17:51 +0500

    feat: expose guarded engineering handler

--- components/web_server/include/engineering_auth.h
+++ components/web_server/include/engineering_auth.h
@@ -12,6 +12,7 @@ esp_err_t engineering_auth_init(void);
 esp_err_t engineering_auth_register(httpd_handle_t server);
 bool engineering_auth_is_authorized(httpd_req_t *request);
 esp_err_t engineering_auth_require(httpd_req_t *request);
+esp_err_t engineering_auth_guarded_handler(httpd_req_t *request);
 
 #ifdef __cplusplus
 }
