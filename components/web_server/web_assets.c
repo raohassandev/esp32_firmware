bd948ae5a3b5bbc0961595fc1d60400e478ac99e
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-27 16:14:51 +0500

    Embed inverter configuration browser module

--- components/web_server/web_assets.c
+++ components/web_server/web_assets.c
@@ -27,6 +27,8 @@ extern const uint8_t devices_refresh_js_start[] asm("_binary_devices_refresh_js_
 extern const uint8_t devices_refresh_js_end[] asm("_binary_devices_refresh_js_end");
 extern const uint8_t inverter_profiles_js_start[] asm("_binary_inverter_profiles_js_start");
 extern const uint8_t inverter_profiles_js_end[] asm("_binary_inverter_profiles_js_end");
+extern const uint8_t inverter_config_js_start[] asm("_binary_inverter_config_js_start");
+extern const uint8_t inverter_config_js_end[] asm("_binary_inverter_config_js_end");
 extern const uint8_t em500_utils_js_start[] asm("_binary_em500_utils_js_start");
 extern const uint8_t em500_utils_js_end[] asm("_binary_em500_utils_js_end");
 extern const uint8_t em500_core_js_start[] asm("_binary_em500_core_js_start");
@@ -57,6 +59,7 @@ const char *web_assets_devices_utils_js(size_t *length) { return asset(devices_u
 const char *web_assets_devices_js(size_t *length) { return asset(devices_js_start, devices_js_end, length); }
 const char *web_assets_devices_refresh_js(size_t *length) { return asset(devices_refresh_js_start, devices_refresh_js_end, length); }
 const char *web_assets_inverter_profiles_js(size_t *length) { return asset(inverter_profiles_js_start, inverter_profiles_js_end, length); }
+const char *web_assets_inverter_config_js(size_t *length) { return asset(inverter_config_js_start, inverter_config_js_end, length); }
 const char *web_assets_em500_utils_js(size_t *length) { return asset(em500_utils_js_start, em500_utils_js_end, length); }
 const char *web_assets_em500_core_js(size_t *length) { return asset(em500_core_js_start, em500_core_js_end, length); }
 const char *web_assets_em500_profiles_js(size_t *length) { return asset(em500_profiles_js_start, em500_profiles_js_end, length); }
