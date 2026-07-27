af2cd107b307eb24eb637cae20e6cde29395318e
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-27 17:08:54 +0500

    feat: declare theme assets

--- components/web_server/include/web_assets.h
+++ components/web_server/include/web_assets.h
@@ -3,10 +3,12 @@
 
 const char *web_assets_index(size_t *length);
 const char *web_assets_css(size_t *length);
+const char *web_assets_theme_css(size_t *length);
 const char *web_assets_wifi_css(size_t *length);
 const char *web_assets_devices_css(size_t *length);
 const char *web_assets_em500_css(size_t *length);
 const char *web_assets_js(size_t *length);
+const char *web_assets_theme_js(size_t *length);
 const char *web_assets_wifi_utils_js(size_t *length);
 const char *web_assets_wifi_guard_js(size_t *length);
 const char *web_assets_wifi_js(size_t *length);
