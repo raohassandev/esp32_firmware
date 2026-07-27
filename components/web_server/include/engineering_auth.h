656d31cb2929d58bdf735b56812af57b48a1da5c
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-27 18:10:35 +0500

    feat: add engineering authentication interface

---
+++ components/web_server/include/engineering_auth.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdbool.h>
+#include "esp_err.h"
+#include "esp_http_server.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+esp_err_t engineering_auth_init(void);
+esp_err_t engineering_auth_register(httpd_handle_t server);
+bool engineering_auth_is_authorized(httpd_req_t *request);
+esp_err_t engineering_auth_require(httpd_req_t *request);
+
+#ifdef __cplusplus
+}
+#endif
