f02af8ce432167d96ad181038c9e39defdf452f4
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-27 17:33:18 +0500

    feat: declare commissioning enhancement asset

--- components/web_server/include/web_assets.h
+++ components/web_server/include/web_assets.h
@@ -9,6 +9,7 @@ const char *web_assets_devices_css(size_t *length);
 const char *web_assets_em500_css(size_t *length);
 const char *web_assets_js(size_t *length);
 const char *web_assets_theme_js(size_t *length);
+const char *web_assets_ui_enhancements_js(size_t *length);
 const char *web_assets_wifi_utils_js(size_t *length);
 const char *web_assets_wifi_guard_js(size_t *length);
 const char *web_assets_wifi_js(size_t *length);
