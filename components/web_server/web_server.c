4ea8457c152d8a226a056fa498f840c7df7fbb2b
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-27 18:51:09 +0500

    feat: serve dedicated operator product view

--- components/web_server/web_server.c
+++ components/web_server/web_server.c
@@ -88,6 +88,7 @@ static esp_err_t js_handler(httpd_req_t *request)
         web_assets_em500_core_js,
         web_assets_em500_profiles_js,
         web_assets_em500_plan_js,
+        web_assets_operator_view_js,
         web_assets_ui_enhancements_js
     };
     return send_asset_parts(request, "application/javascript; charset=utf-8",
