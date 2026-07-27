074c2558b754171dbac4fcc9709252c47340c059
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-27 18:25:44 +0500

    feat: expose registration security gateway

--- components/web_server/include/engineering_auth.h
+++ components/web_server/include/engineering_auth.h
@@ -13,6 +13,8 @@ esp_err_t engineering_auth_register(httpd_handle_t server);
 bool engineering_auth_is_authorized(httpd_req_t *request);
 esp_err_t engineering_auth_require(httpd_req_t *request);
 esp_err_t engineering_auth_guarded_handler(httpd_req_t *request);
+esp_err_t engineering_register_uri_handler(httpd_handle_t server,
+                                           const httpd_uri_t *uri_handler);
 
 #ifdef __cplusplus
 }
