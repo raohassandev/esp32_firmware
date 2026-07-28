ea42b31164d5bd71a0d55d21ff614a49579fb446
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-28 17:25:23 +0500

    Expose meter communication quality and backoff state

--- components/meter_manager/include/meter_types.h
+++ components/meter_manager/include/meter_types.h
@@ -5,11 +5,16 @@
 typedef struct {
     float active_power_kw;
     bool online;
+    bool degraded;
     bool connection_initialized;
     uint32_t last_update_ms;
     uint32_t last_attempt_ms;
+    uint32_t last_response_time_ms;
     uint32_t success_count;
     uint32_t response_errors;
     uint32_t consecutive_failures;
+    uint32_t current_poll_delay_ms;
+    uint8_t recent_sample_count;
+    uint8_t recent_success_percent;
     int32_t last_error;
 } meter_data_t;
