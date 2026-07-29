543b1da69285e0e55de9630b0c675dbc67920429
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-29 10:25:37 +0500

    Add persisted solar-grid configuration model

---
+++ components/solar_grid_config/include/solar_grid_config.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "esp_err.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SOLAR_GRID_CONFIG_MAGIC 0x53475244u
+#define SOLAR_GRID_CONFIG_VERSION 1u
+
+typedef enum {
+    SOLAR_GRID_POLICY_ZERO_EXPORT = 0,
+    SOLAR_GRID_POLICY_LIMITED_EXPORT,
+    SOLAR_GRID_POLICY_MINIMUM_IMPORT
+} solar_grid_policy_t;
+
+typedef enum {
+    SOLAR_GRID_IMPORT_POSITIVE = 0,
+    SOLAR_GRID_EXPORT_POSITIVE
+} solar_grid_meter_orientation_t;
+
+typedef struct {
+    bool enabled;
+    uint8_t meter_index;
+    uint8_t function_code;
+    uint16_t address;
+    uint16_t mask;
+    uint16_t active_value;
+} solar_grid_signal_config_t;
+
+typedef struct {
+    uint32_t magic;
+    uint16_t version;
+    solar_grid_policy_t policy;
+    solar_grid_meter_orientation_t meter_orientation;
+    float export_limit_kw;
+    float minimum_import_kw;
+    solar_grid_signal_config_t grid_available;
+    solar_grid_signal_config_t grid_breaker_closed;
+    uint32_t evidence_poll_interval_ms;
+    uint32_t evidence_stale_timeout_ms;
+    uint32_t grid_loss_trip_ms;
+    uint32_t grid_recovery_stable_ms;
+} solar_grid_config_t;
+
+esp_err_t solar_grid_config_init(void);
+esp_err_t solar_grid_config_get_snapshot(solar_grid_config_t *out_config);
+esp_err_t solar_grid_config_save(const solar_grid_config_t *config);
+void solar_grid_config_defaults(solar_grid_config_t *config);
+bool solar_grid_config_valid(const solar_grid_config_t *config);
+bool solar_grid_config_evidence_complete(const solar_grid_config_t *config);
+const char *solar_grid_policy_name(solar_grid_policy_t policy);
+const char *solar_grid_orientation_name(solar_grid_meter_orientation_t orientation);
+
+#ifdef __cplusplus
+}
+#endif
