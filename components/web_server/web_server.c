1e800dc43cbd27a1a704e9129e5ab03d5e3d3cc2
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-07-30 19:44:22 +0500

    feat(web): load industrial operator UI last

--- components/web_server/web_server.c
+++ components/web_server/web_server.c
@@ -83,7 +83,8 @@ static esp_err_t css_handler(httpd_req_t *request)
         web_assets_product_shell_v2_css,
         web_assets_product_experience_v2_css,
         web_assets_commissioning_wizard_v2_css,
-        web_assets_commissioning_release_v3_css
+        web_assets_commissioning_release_v3_css,
+        web_assets_industrial_operator_ui_css
     };
     return send_asset_parts(request, "text/css; charset=utf-8", assets, sizeof(assets) / sizeof(assets[0]));
 }
@@ -121,7 +122,8 @@ static esp_err_t js_handler(httpd_req_t *request)
         web_assets_ui_enhancements_js,
         web_assets_product_shell_v2_js,
         web_assets_product_experience_v2_js,
-        web_assets_commissioning_release_v3_js
+        web_assets_commissioning_release_v3_js,
+        web_assets_industrial_operator_ui_js
     };
     return send_asset_parts(request, "application/javascript; charset=utf-8", assets, sizeof(assets) / sizeof(assets[0]));
 }
