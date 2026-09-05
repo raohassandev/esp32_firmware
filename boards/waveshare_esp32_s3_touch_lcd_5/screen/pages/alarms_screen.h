67317a4d088127b6ef27edb534e23c4cc37430a4
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-09-05 10:15:11 +0500

    Waveshare: define authenticated alarm acknowledgement callback

--- boards/waveshare_esp32_s3_touch_lcd_5/screen/pages/alarms_screen.h
+++ boards/waveshare_esp32_s3_touch_lcd_5/screen/pages/alarms_screen.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
 #include "lvgl.h"
 #include "screen_api.h"
 
@@ -7,7 +11,14 @@
 extern "C" {
 #endif
 
+typedef bool (*alarms_screen_acknowledge_fn)(void *context,
+                                              uint32_t code,
+                                              char *message,
+                                              size_t message_capacity);
+
 lv_obj_t *alarms_screen_create(lv_obj_t *parent);
+void alarms_screen_set_acknowledge_backend(alarms_screen_acknowledge_fn acknowledge,
+                                           void *context);
 void alarms_screen_apply_alarms(const screen_alarms_snapshot_t *snapshot);
 void alarms_screen_apply_events(const screen_events_snapshot_t *snapshot);
 void alarms_screen_show_unavailable(void);
