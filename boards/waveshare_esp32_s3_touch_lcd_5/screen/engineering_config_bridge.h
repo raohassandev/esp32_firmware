8edb532c36b51c830e2135605734102b6e19681c
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-09-16 18:43:36 +0500

    feat(engineering): add native configuration section bridge

---
+++ boards/waveshare_esp32_s3_touch_lcd_5/screen/engineering_config_bridge.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "pvdg_ui_export_control.h"
+#include "pvdg_ui_inverter_setup.h"
+#include "pvdg_ui_meter_setup.h"
+#include "pvdg_ui_source_setup.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void engineering_config_source_request(void *user);
+void engineering_config_source_submit(const pvdg_ui_source_config_t *config, void *user);
+void engineering_config_meter_request(void *user);
+void engineering_config_meter_submit(const pvdg_ui_meter_list_t *list, void *user);
+void engineering_config_inverter_request(void *user);
+void engineering_config_inverter_submit(const pvdg_ui_inverter_list_t *list, void *user);
+void engineering_config_export_request(void *user);
+void engineering_config_export_submit(const pvdg_ui_source_config_t *config, void *user);
+
+#ifdef __cplusplus
+}
+#endif
