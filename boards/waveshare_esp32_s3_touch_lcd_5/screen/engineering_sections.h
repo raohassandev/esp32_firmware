968083a833af37b9cc66ce08dd57903dad1087f7
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-09-16 18:45:13 +0500

    feat(engineering): add section router

---
+++ boards/waveshare_esp32_s3_touch_lcd_5/screen/engineering_sections.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "pvdg_ui_engineering.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void engineering_sections_open(pvdg_ui_engineering_section_t section);
+void engineering_sections_hide(void);
+
+#ifdef __cplusplus
+}
+#endif
