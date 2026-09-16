0c50fb70d1e1614f6b38b347bc358a93be5fd61e
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-09-16 19:06:45 +0500

    feat(engineering): expose inverter profile callbacks

--- boards/waveshare_esp32_s3_touch_lcd_5/screen/engineering_config_bridge.h
+++ boards/waveshare_esp32_s3_touch_lcd_5/screen/engineering_config_bridge.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 #include "pvdg_ui_export_control.h"
 #include "pvdg_ui_inverter_setup.h"
 #include "pvdg_ui_meter_setup.h"
@@ -14,7 +16,12 @@ void engineering_config_source_submit(const pvdg_ui_source_config_t *config, voi
 void engineering_config_meter_request(void *user);
 void engineering_config_meter_submit(const pvdg_ui_meter_list_t *list, void *user);
 void engineering_config_inverter_request(void *user);
+void engineering_config_inverter_profiles_request(void *user);
+void engineering_config_inverter_assignments_request(void *user);
 void engineering_config_inverter_submit(const pvdg_ui_inverter_list_t *list, void *user);
+void engineering_config_inverter_assign_profile(uint8_t inverter_index,
+                                                const char *profile_id,
+                                                void *user);
 void engineering_config_export_request(void *user);
 void engineering_config_export_submit(const pvdg_ui_source_config_t *config, void *user);
 
